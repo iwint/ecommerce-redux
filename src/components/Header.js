f9de50b6a1e9070ec519cdc1a70a5ad04b447497
Gobinath <princegopinath42@gmail.com>
2023-09-19 00:24:34 +0530

    refactor: Header home route

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Header() {
+    const navigate = useNavigate();
   return (
     <div className='p-3 bg-secondary'>
       <h3>Pandian Stores</h3>
          <div className='row justify-content-center'>
           <div className='col-sm-12 col-md-7 col-lg-6 col-xl-5 d-flex'>
-            <button className='btn btn-primary'>Home </button>
+            <button className='btn btn-primary me-4' onClick={()=>{navigate('/')}}>Home </button>
             <input className='form-control' type='search' placeholder='search the item' />
-            <button className='btn btn-primary'>Cart </button>
+            <button className='btn btn-primary ms-4'>Cart </button>
           </div>
          </div>
     </div>
