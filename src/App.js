dc8add127e257f4554b267f933862308834612b5
Gobinath B <princegopinath42@gmail.com>
2023-09-24 23:42:27 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Router from "./routes";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Provider } from "react-redux";
+import store from "./redux/store";
 function App() {
   return (
     <div className="App">
+      <Provider store={store} />
       <Router/>
     </div>
   );
