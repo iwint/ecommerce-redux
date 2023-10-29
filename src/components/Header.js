df17e46fb9fd626cc10e21906f0b9b8c4e2b9015
Gobinath B <princegopinath42@gmail.com>
2023-10-29 21:50:21 +0530

    chore

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     const navigate = useNavigate();
   return (
     <div className='p-3 bg-primary'>
-      <h3 style={{color:'white'}}>Pandian Stores</h3>
+      <h3 style={{color:'white'}}>Bad Mobiles</h3>
          <div className='row justify-content-center'>
           <div className='col-sm-12 col-md-7 col-lg-6 col-xl-5 d-flex'>
             <button className='btn btn-warning me-4' onClick={()=>{navigate('/')}}>Home </button>
