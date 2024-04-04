4e2ff1be6cb68d2284ad0b433e6d139f8107c5f2
Gobinath B <princegopinath42@gmail.com>
2024-04-04 22:49:42 +0530

    ref store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
+
 import {configureStore} from "@reduxjs/toolkit";
+import cartReducer from './reducer/cart';
 
 export default configureStore({
      reducer:{
-
+     cart: cartReducer
      }
 });
