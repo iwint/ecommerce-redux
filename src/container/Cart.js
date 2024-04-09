cbbe58cd904cc28c586dfa8a2ca99cf853e0d293
Gobinath B <princegopinath42@gmail.com>
2024-04-09 23:04:44 +0530

    feat cart

--- src/container/Cart.js
+++ src/container/Cart.js
@@ -1,9 +1,15 @@
+/* eslint-disable array-callback-return */
 import React from 'react'
-
+import { useSelector } from 'react-redux';
+import ProductListItem from '../components/ProductListItem';
 export default function Cart() {
+    const list = useSelector((state)=>state.cart.list);
   return (
-    <div>
-      
-    </div>
+    <>
+    {list.map((item) => (
+    <ProductListItem {...item} key={item.id} />
+))}
+
+    </>
   )
 }
