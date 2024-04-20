4280955b39de73409d0a72417ee31050bbe0fb3d
Gobinath B <princegopinath42@gmail.com>
2024-04-20 15:48:01 +0530

    auth routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Home from '../container/Home';
+import Login from '../container/Login';
+import { AuthProvider } from './authProvider';
+
 export default function Router() {
+
   return (
+    <AuthProvider>
     <BrowserRouter>
        <Routes>
+         <Route path="/login" element={<Login />} />
          <Route path='*'  element={<Home />} />
        </Routes>
     </BrowserRouter>
+    </AuthProvider>
   )
 }
