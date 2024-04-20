29b249a2542d84d2e1db012271717df5dda94309
Gobinath B <princegopinath42@gmail.com>
2024-04-20 13:00:23 +0530

    refactor product lst for cart

--- src/components/ProductListItem.js
+++ src/components/ProductListItem.js
@@ -1,18 +1,24 @@
-import React from 'react'
-
+import React from "react";
 
 export default function ProductListItem(props) {
-
   return (
-    <div className='d-flex mt-2 '>
-          <img src= {props.thumbnail} height={180} width={150} alt={props.title} className='border-radius-9' />
-         <div className='card-title'>{props.title}</div> 
-         <h6 className='mt-2 me-4'>Price: {`$${props.price}`}</h6>
-         <h6 className='mt-2 me-4'>Discount: {`$${props.discountPercentage}`}%</h6>
-         <h6 className='mt-2 me-4'>Rating: {`$${props.rating}`}/5</h6>
-         <div className='mt-4 me-4'>
-            {props.stock > 0 ? <button className='btn btn-success'>Available</button>: <button className='btn btn-outline-danger'>Out of stock</button>}
-         </div>
+    <div className="d-flex mt-2 align-items-center justify-content">
+      <img
+        src={props.thumbnail}
+        height={180}
+        width={150}
+        alt={props.title}
+        className="border-radius-9 me-4"
+      />
+      <h5 className="card-title me-4">{props.title}</h5>
+      <h6 className="mt-2 me-4">Price: {`$${props.price}`}</h6>
+      <h6 className="mt-2 me-4">Discount: {`$${props.discountPercentage}`}%</h6>
+      <h6 className="mt-2 me-4">Rating: {`$${props.rating}`}/5</h6>
+
+      <button className="btn btn-success ms-3" onClick={props.incrementItem}>+</button>
+      <span className="ms-3">Quantity{props.count}</span>
+      <button className="btn btn-danger ms-3" onClick={props.decrementItem}>-</button>
+      <button className="btn btn-danger ms-3" onClick={props.removeItem}>Remove</button>
     </div>
-  )
+  );
 }
