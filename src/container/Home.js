3667a63411b10ae4c1013e150cd7e452021e7fdb
Gobinath B <princegopinath42@gmail.com>
2024-04-20 13:01:09 +0530

    added route for checkout and success

--- src/container/Home.js
+++ src/container/Home.js
@@ -4,6 +4,8 @@ import Header from '../components/Header';
 import Dashboard from './Dashboard';
 import Product from './Product';
 import Cart from './Cart';
+import Checkout from './Checkout';
+import Success from './Success';
 export default function Home() {
   return (
     <div >
@@ -12,6 +14,11 @@ export default function Home() {
         <Route path='/' element={<Dashboard />} />
         <Route path='/product/:id' element={<Product />} />
         <Route path='/cart' element={<Cart/>}/>
+        <Route path='/checkout'>
+          <Route path='' element={<Checkout/>}/>
+          <Route path=':id' element={<Checkout/>}/>
+        </Route>
+        <Route path='success' element={<Success/>}/>
       </Routes>
     </div>
   )
